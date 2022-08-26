9cc5eae22ce9bd1b51e09e598b77aa07591ae5a0
Francisco José Lavalle <franlavalle@hotmail.com>
2022-08-26 01:19:00 -0300

    Todo finalizado, faltan detalles

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 const workContainer = document.querySelector('.workContainer');
+const burga = document.querySelector('.burga');
+const fondoNav = document.querySelector('.fondoNav');
+const navLinks = document.querySelectorAll('.navLinks');
+let useLinks;
 
 let works = [
-    {photo: '../images/torta-1.png', text: 'torta'},
-    {photo: '../images/torta-2.png', text: 'torta'},
-    {photo: '../images/torta-1.png', text: 'torta'},
-    {photo: '../images/torta-2.png', text: 'torta'},
-    {photo: '../images/torta-1.png', text: 'torta'},
-    {photo: '../images/torta-2.png', text: 'torta'}
+    { photo: '../images/torta-1.png', text: 'torta' },
+    { photo: '../images/torta-2.png', text: 'torta' },
+    { photo: '../images/torta-1.png', text: 'torta' },
+    { photo: '../images/torta-2.png', text: 'torta' },
+    { photo: '../images/torta-1.png', text: 'torta' },
+    { photo: '../images/torta-2.png', text: 'torta' }
 ];
 
 works.forEach(work => {
@@ -23,3 +27,27 @@ works.forEach(work => {
     div.append(span);
     workContainer.append(div);
 })
+
+burga.addEventListener('click', openNav)
+fondoNav.addEventListener('click', openNav)
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', () => {
+        if(fondoNav.classList.contains('displayNav')) {
+            openNav();
+        }
+    })
+}
+
+function openNav() {
+    const burgas = document.querySelectorAll('.burgas');
+    const navBar = document.querySelector(".header__nav-ul");
+    useLinks = document.querySelectorAll('.useLinks');
+    const body = document.querySelector('body');
+
+    burgas[0].classList.toggle('burga0');
+    burgas[1].classList.toggle('burga1');
+    burgas[2].classList.toggle('burga2');
+    navBar.classList.toggle("nav-menu_visible");
+    fondoNav.classList.toggle('displayNav');
+    body.classList.toggle('no-scroll');
+}
